cf6a0f5a46905270d6c91725a57d8b932cffcbb9
gomaiwasaki8 <ykh2135008@stu.o-hara.ac.jp>
2022-09-15 12:35:37 +0900

    P285くらい日記作成機能追加

--- diary/urls.py
+++ diary/urls.py
@@ -4,7 +4,8 @@ app_name = 'diary'
 urlpatterns = [
     # ルーティングの仕組みに名前を付けているname。間違えるとエラー。
     path('', views.IndexView.as_view(), name = "index"),
-    path('inquiry/', views.InquiryView.as_view(), name = "inquiry"),
-    path('diary-list/', views.DiaryListView.as_view(), name = 'diary_list'),
-
+    path('inquiry/', views.InquiryView.as_view(), name = "inquiry"), # お問い合わせ
+    path('diary-list/', views.DiaryListView.as_view(), name = 'diary_list'), # 日記の一覧
+    path('diary-detail/<int:pk>/', views.DiaryDetailView.as_view(), name = 'diary_detail'), # 日記詳細表示
+    path('diary-create/', views.DiaryCreateView.as_view(), name = 'diary_create'), # 日記作成機能
 ]
--- diary/views.py
+++ diary/views.py
@@ -1,7 +1,7 @@
 from django.views import generic
 import logging
 from django.urls import reverse_lazy
-from .forms import InquiryForm
+from .forms import InquiryForm, DiaryCreateForm
 logger = logging.getLogger(__name__)
 from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
@@ -30,8 +30,32 @@ class InquiryView(generic.FormView):
 class DiaryListView(LoginRequiredMixin, generic.ListView):
     model = Diary
     template_name = 'diary_list.html'
+    paginate_by = 2 # 数字は多分表示する数
 
     # メソッドのオーバーライド。filterとは抽出。自分が登録した日記を自分だけが見れるという前提だからこうなっている。order_byは表示する順番。
     def get_queryset(self):
         diaries = Diary.objects.filter(user = self.request.user).order_by('-created_at')
         return diaries
+
+# 日記詳細表示のクラス
+class DiaryDetailView(LoginRequiredMixin, generic.DetailView):
+    # 詳細画面を表示するにはモデル（データベースと連携させるモデル）が必要
+    model = Diary
+    template_name = 'diary_detail.html'
+
+class DiaryCreateView(LoginRequiredMixin, generic.CreateView):
+    model = Diary
+    template_name = 'diary_create.html'
+    form_class = DiaryCreateForm
+    success_url = reverse_lazy('diary:diary_list')
+
+    def form_valid(self, form): # 登録が成功した時の処理。formはユーザが入力したのが入っている
+        diary = form.save(commit = False) # 日記をセーブ（登録）する。commit=Falseはまだすべての情報が入っていないからコミットしないって意味
+        diary.user = self.request.user # ユーザ名を入れている（ユーザが入力しないでいいようにこっちでユーザ名をセットする）
+        diary.save()
+        messages.success(self.request, "日記を作成しました。")
+        return super().form_valid(form)
+
+    def form_invalid(self, form):
+        messages.error(self.request, "日記の作成に失敗しました。")
+        return super().form_invalid(form)
